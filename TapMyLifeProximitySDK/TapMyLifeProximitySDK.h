391e9e57003e9e4b9d7ae37462e86ed27b4fb4dd
Ivan Vecchi <ivanvecchi@ivans-mac-mini.local>
2016-03-09 10:10:32 +0100

    Initial Commit

---
+++ TapMyLifeProximitySDK/TapMyLifeProximitySDK.h
@@ -0,0 +1,19 @@
+//
+//  TapMyLifeProximitySDK.h
+//  TapMyLifeProximitySDK
+//
+//  Created by Ivan Vecchi on 09/03/16.
+//  Copyright © 2016 TapMyLife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TapMyLifeProximitySDK.
+FOUNDATION_EXPORT double TapMyLifeProximitySDKVersionNumber;
+
+//! Project version string for TapMyLifeProximitySDK.
+FOUNDATION_EXPORT const unsigned char TapMyLifeProximitySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapMyLifeProximitySDK/PublicHeader.h>
+
+
